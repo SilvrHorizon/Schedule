cfa3c4f1c8e39bf691aee320b978c207564657fc
SilvrHorison <jonathan.godar@gmail.com>
2020-07-10 23:29:45 +0200

    Updates to the database

--- app/models.py
+++ app/models.py
@@ -1,17 +1,70 @@
-from app import db
 from flask_login import UserMixin
+
+from app import login
+from app import db
+
+from customUtilities.time import format_minutes
+
+
+followers = db.Table('followers', db.Column('follower_id', db.Integer, db.ForeignKey('user.id')), db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     google_id = db.Column(db.String(32), index=True)
-    username = db.Column(db.String(64), index=True)
+    first_name = db.Column(db.String(64), index=True)
+    last_name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(64), index=True, unique=True)
 
-    #followers = db.Table('followers', db.Column('follower_id', db.Integer, db.Foreign_key('user.id')), db.Column('followed_id', db.Integer, db.Foreign_key('user.id')))
-    #follows = db.relationship('User', secondary=followers, primary_join=(followers.c.follower_id == id), secondary_join=(followers.c.followed_id == id), backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    follows = db.relationship('User', secondary=followers, primaryjoin=(followers.c.follower_id == id), secondaryjoin=(followers.c.followed_id == id), backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
+    schedules = db.relationship('Schedule', backref="user", lazy='dynamic')
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.follows.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.follows.remove(user)
+    
+    def is_following(self, user):
+        return self.follows.filter(followers.c.followed_id == user.id).count() > 0 
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
-    
 
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    week_number = db.Column(db.SmallInteger, index=True)
+    classes = db.relationship('Class', backref="schedule", lazy='dynamic')
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    days_binary_representations = db.relationship('DayBinaryRepresentation', backref='schedule', lazy='dynamic')
+
+    def __repr__(self):
+        return "<Schedule {}>".format(self.week_number)
+
+class DayBinaryRepresentation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    weekday = db.Column(db.Integer)
+
+    # A 0 represents free time and a 1 represents lessons
+    value = db.Column(db.LargeBinary(length=128))
+    schedule_id = db.Column(db.Integer, db.ForeignKey('schedule.id'))
+
+class Class(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course = db.Column(db.String(16))
+    weekday = db.Column(db.SmallInteger, index=True)
+    begins = db.Column(db.Integer, index=True)
+    ends = db.Column(db.Integer, index=True)
+
+    schedule_id = db.Column(db.Integer, db.ForeignKey('schedule.id'))
+
+    def __repr__(self):
+        return "<Class [course:{}, weekday:{} begins:{}, ends:{}]>".format(self.course, self.weekday, format_minutes(self.begins), format_minutes(self.ends))
+
+@login.user_loader
+def user_loader(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,55 +1,163 @@
-from flask import render_template, request
+# Flask related imports
+from flask import render_template, request, url_for, redirect, abort
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+
+# For Google oAuth
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# For image processing
 import numpy as np
 import cv2
+from PIL import Image
 
+# General
+import json
+
+from config import Config, get_google_provider_config
+from app import db
+from app.models import User, Schedule, Class, DayBinaryRepresentation
 from app import App
 from ScheduleImageProcessor import findCourses
+from customUtilities.time import hour_minute_to_minutes, format_minutes
+from customUtilities.schedulemanip import getBinaryRepresentation
 
-from PIL import Image
-from io import BytesIO
-
-
-
+# oAuth2 client setup
+oauthClient = WebApplicationClient(Config.GOOGLE_CLIENT_ID)
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Move to a better place
 def sortFunction(e):
     try:
-        return 3600 * e.weekDay + e.begins[0] * 60 + e.begins[1]
+        return 60 * 24 * e.weekday + e.begins
     except:
-        return 10000
+        return 100000
 
+@login_required
 @App.route('/upload-schedule', methods=['GET', 'POST'])
 def uploadSchedule():
+
     if request.method == 'POST':
         if request.files['image']:
+            
+            # Get courses
             img = Image.open(request.files['image'].stream).convert('RGB')
             opencvimg = np.array(img)
             courses = findCourses(cv2.cvtColor(opencvimg, cv2.COLOR_BGR2GRAY), debug=False)
-            print("Found a total of {} courses".format(len(courses)))
-
             courses.sort(key=sortFunction)
-            print(courses)
+
+            schedule = None
+
+            storedSchedule = Schedule.query.filter_by(user=current_user, week_number=-1).first()
+            if storedSchedule is not None:
+                Class.query.filter_by(schedule=storedSchedule).delete()
+                db.session.commit()
+                schedule = storedSchedule
+            else:
+                schedule = Schedule(week_number=-1, user=current_user)
+                db.session.add(schedule)
+            
+            course_by_day = [[], [], [], [], []]
+
+            for course in courses:
+                compiled = Class(begins=course.begins, ends=course.ends, course=course.courseName, weekday=course.weekday, schedule=schedule)
+                course_by_day[course.weekday].append(course)
+                db.session.add(compiled)
+
+            for i in range(len(course_by_day)):
+                rep = getBinaryRepresentation(course_by_day[i]).to_bytes(128, "big")
+                print("i:", i)
+                print(rep)
+                db.session.add(DayBinaryRepresentation(schedule=schedule, value=rep, weekday=i))
+
+            db.session.commit()
 
             builder = '<h1>Schemat</h1>'
             days = {0: "Måndag", 1: "Tisdag", 2: "Onsdag", 3: "Torsdag", 4: "Fredag"}
             for course in courses:
                 builder += '<p>'
                 try:
-                    builder += '{} på {} kl: {:02d}:{:02d} till {:02d}:{:02d}'.format(course.courseName, days[course.weekDay], course.begins[0], course.begins[1], course.ends[0], course.ends[1])
+                    builder += '{} på {} kl: {} till {}'.format(course.courseName, days[course.weekday], format_minutes(course.begins), format_minutes(course.ends))
                 except:
                     builder += 'En kurs fick innehåller inte nog med data för att visas'
                 builder += '</p>'
-            return builder
+            return builder + "<p>{}</p>".format(getBinaryRepresentation(courses))
         else:
             return 'Did not get image'
- 
+
 
     return render_template('upload-schedule.html')
 
 @App.route('/login')
 def login():
-    pass
+    authorization_endpoint = get_google_provider_config()["authorization_endpoint"]
+
+    request_uri = oauthClient.prepare_request_uri(authorization_endpoint, redirect_uri=request.base_url + "/callback", scope=["openid", "email", "profile"])
+
+    return redirect(request_uri)
+
+
+@App.route('/')
+def index():
+    if current_user.is_authenticated:
+        return "logged in as {}".format(current_user.email)
+    else:
+        return "Index"
+
+
+@App.route('/login/callback')
+def login_callback():
+    code = request.args.get("code")
+
+    google_config = get_google_provider_config()
+    token_endpoint = google_config["token_endpoint"]
+
+    token_url, headers, body = oauthClient.prepare_token_request(token_endpoint, authorization_response=request.url, redirect_url=request.base_url, code=code)
+
+    token_response = requests.post(token_url, headers=headers, data=body, auth=(Config.GOOGLE_CLIENT_ID, Config.GOOGLE_CLIENT_SECRET))
+
+    oauthClient.parse_request_body_response(json.dumps(token_response.json()))
+
+    userinfo_endpoint = google_config["userinfo_endpoint"]
+    uri, headers, body = oauthClient.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+
+    if userinfo_response.json().get("email_verified"):
+        google_id = userinfo_response.json()["sub"]
+        print(google_id)
+        user = User.query.filter_by(google_id=google_id).first()
+
+        if user is not None:
+            login_user(user, True)
+        else:
+            u = User(first_name=userinfo_response.json()["given_name"], last_name=userinfo_response.json()["family_name"], email=userinfo_response.json()["email"], google_id=userinfo_response.json()["sub"])
+            db.session.add(u)
+            db.session.commit()
+            login_user(u, True)
+    else:
+        return "Email not verified!"
+
+    return redirect(url_for('index'))
+
+@login_required
+@App.route('/my-schedule', methods=['GET'])
+def show_schedule():
+    week_number = request.args.get("week_number")
+    if week_number:
+        week_number = int(week_number)
+
+        schedule = Schedule.query.filter_by(user=current_user, week_number=week_number).first()
+        if not schedule:
+            schedule = Schedule.query.filter_by(user=current_user, week_number=week_number).first()
+        if not schedule:
+            abort(404, "Du har inget standard schema inlagt")
+
+        bin = getBinaryRepresentation(Class.query.filter_by(schedule=schedule, weekday=0).all())
+        return bin
+
+    return render_template('my-schedule.html')
